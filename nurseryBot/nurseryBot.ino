894a2823d352c9c80cd41a0ca36e2879ff415fb5
John-Khalil <johnadelkhalil@gmail.com>
2023-02-09 03:43:37 +0200

    servo test

--- nurseryBot/nurseryBot.ino
+++ nurseryBot/nurseryBot.ino
@@ -2,11 +2,13 @@
 #include <WebServer.h>
 #include <WebSocketsServer.h>
 #include "src/basics.h"
+
+#include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
 Adafruit_PWMServoDriver servoModules[]={Adafruit_PWMServoDriver(0x40),Adafruit_PWMServoDriver(0x41)};
 #define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVOMAX  650 // this is the 'maximum' pulse length count (out of 4096)
 
 void setServo(uint16_t servoMotor,uint16_t angle){
   servoModules[servoMotor/16].setPWM((servoMotor%16),0,map(angle,0, 180, SERVOMIN,SERVOMAX));
@@ -29,16 +31,23 @@ WebSocketsServer webSocket = WebSocketsServer(81);
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+  servoModuleInit();
+  while(1){
+    setServo(13,0);
+    delay(500);
+    setServo(13,180);
+    delay(500);
   }
-  Serial.println("Connected to WiFi");
+  // WiFi.begin(ssid, password);
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(1000);
+  //   Serial.println("Connecting to WiFi...");
+  // }
+  // Serial.println("Connected to WiFi");
 
-  server.begin();
-  webSocket.begin();
-  webSocket.onEvent(webSocketEvent);
+  // server.begin();
+  // webSocket.begin();
+  // webSocket.onEvent(webSocketEvent);
 }
 
 void loop() {
@@ -60,3 +69,63 @@ void webSocketEvent(uint8_t client_num, WStype_t type, uint8_t * payload, size_t
       break;
   }
 }
+
+// #include <Wire.h>
+
+// #include <Adafruit_PWMServoDriver.h>
+
+// // called this way, it uses the default address 0x40
+// Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40);
+// Adafruit_PWMServoDriver board2 = Adafruit_PWMServoDriver(0x41);
+
+// // Depending on your servo make, the pulse width min and max may vary, you 
+// // want these to be as small/large as possible without hitting the hard stop
+// // for max range. You'll have to tweak them as necessary to match the servos you
+// // have!
+// // Watch video V1 to understand the two lines below: http://youtu.be/y8X9X10Tn1k
+// #define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
+// #define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
+
+
+// int servoNumber = 0;
+
+// void setup() {
+//   Serial.begin(9600);
+//   Serial.println("32 channel Servo test!");
+
+//   board1.begin();
+//   board2.begin();  
+//   board1.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+//   board2.setPWMFreq(60);
+//   //yield();
+// }
+
+// // the code inside loop() has been updated by Robojax
+// void loop() {
+
+
+//     for( int angle =0; angle<181; angle +=10){
+//       for(int i=0; i<16; i++)
+//         {      
+//             board2.setPWM(i, 0, angleToPulse(angle) );
+//             board1.setPWM(i, 0, angleToPulse(angle) );
+//         }
+//     }
+  
+// // robojax PCA9865 16 channel Servo control
+//   delay(100);
+ 
+// }
+
+// /*
+//  * angleToPulse(int ang)
+//  * gets angle in degree and returns the pulse width
+//  * also prints the value on seial monitor
+//  * written by Ahmad Nejrabi for Robojax, Robojax.com
+//  */
+// int angleToPulse(int ang){
+//    int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max 
+//    Serial.print("Angle: ");Serial.print(ang);
+//    Serial.print(" pulse: ");Serial.println(pulse);
+//    return pulse;
+// }
--- simple servo test.py
+++ simple servo test.py
@@ -1,9 +1,53 @@
+# import websocket
+# import json
+# import time
+
+# def on_message(ws, message):
+#     print("Received message: ", message)
+
+# def on_error(ws, error):
+#     print("Error: ", error)
+
+# def on_close(ws):
+#     print("Connection closed")
+
+# def on_open(ws):
+#     while True:
+#         message = {"instruction":1024,"servoPosition":2400}
+#         ws.send("data:"+json.dumps(message)+"<@>")
+#         time.sleep(5)
+#         message = {"instruction":1024,"servoPosition":500}
+#         ws.send("data:"+json.dumps(message)+"<@>")
+#         time.sleep(5)
+
+
+# if __name__ == "__main__":
+#     websocket.enableTrace(True)
+#     ws = websocket.WebSocketApp("ws://192.168.1.18:80",
+#                               on_message = on_message,
+#                               on_error = on_error,
+#                               on_close = on_close)
+#     ws.on_open = on_open
+#     ws.run_forever()
+
 import websocket
 import json
 import time
 
+
+
+
 def on_message(ws, message):
-    print("Received message: ", message)
+    print("--------Received message: ", message)
+    # Deserialize the received JSON message
+    # received_message = json.loads(message)
+
+    # # Extract values from the JSON object
+    # servo = received_message["medo"]
+
+    # # Print the extracted values
+    # print("medo:", servo)
+ 
 
 def on_error(ws, error):
     print("Error: ", error)
@@ -12,20 +56,27 @@ def on_close(ws):
     print("Connection closed")
 
 def on_open(ws):
-    while True:
-        message = {"instruction":1024,"servoPosition":2400}
-        ws.send("data:"+json.dumps(message)+"<@>")
-        time.sleep(5)
-        message = {"instruction":1024,"servoPosition":500}
-        ws.send("data:"+json.dumps(message)+"<@>")
-        time.sleep(5)
+    message = {"auth":"test0"}
+    ws.send(json.dumps(message))
+    # while True:
+    input_text =json.dumps({"servo":1024,"pos":0,"pos2":180})
+    ws.send(""+input_text+"")
 
 
-if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://192.168.1.18:80",
-                              on_message = on_message,
-                              on_error = on_error,
-                              on_close = on_close)
-    ws.on_open = on_open
-    ws.run_forever()
+
+# if name == "main":
+#     websocket.enableTrace(True)
+#     ws = websocket.WebSocketApp("ws://xtensa32plus.ddns.net:51",
+#                               on_message = on_message,
+#                               on_error = on_error,
+#                               on_close = on_close)
+#     ws.on_open = on_open ##law ha receive yeb2a : on_message 
+#     ws.run_forever()
+websocket.enableTrace(True)
+ws = websocket.WebSocketApp("ws://xtensa32plus.ddns.net:51",
+                            on_message = on_message,
+                            on_error = on_error,
+                            on_close = on_close)
+ws.on_open = on_open ##law ha receive yeb2a : on_message 
+ws.run_forever()
+
